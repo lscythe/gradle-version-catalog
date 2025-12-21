3be0baec8e653abf60903605259f142279ec0edf
Lscythe <lscythe.git@proton.me>
2025-12-21 17:46:20 +0700

    release: 2025.12.21

--- build-logic/conventions/src/main/kotlin/convention.publish.gradle.kts
+++ build-logic/conventions/src/main/kotlin/convention.publish.gradle.kts
@@ -8,32 +8,35 @@ val version = providers.environmentVariable("VERSION")
     .orElse("0.0.0-SNAPSHOT")
 
 mavenPublishing {
+    publishToMavenCentral(automaticRelease = true, validateDeployment = true)
+    signAllPublications()
     coordinates(
         groupId = providers.gradleProperty("GROUP").get(),
         artifactId = project.name,
         version = version.get()
     )
-    
+
     pom {
         name.set(project.name)
         description.set(project.description ?: "Lscythe Version Catalog")
         url.set("https://github.com/lscythe/gradle-version-catalog")
         inceptionYear.set("2025")
-        
+
         licenses {
             license {
                 name.set("MIT License")
                 url.set("https://opensource.org/licenses/MIT")
             }
         }
-        
+
         developers {
             developer {
                 id.set("lscythe")
                 name.set("Lscythe")
+                url.set("https://github.com/lscythe")
             }
         }
-        
+
         scm {
             url.set("https://github.com/lscythe/gradle-version-catalog")
             connection.set("scm:git:git://github.com/lscythe/gradle-version-catalog.git")
