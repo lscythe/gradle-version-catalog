f9c01459d4edeb1f491824b1fd2c4a1535de9158
Lscythe <lscythe.git@proton.me>
2026-01-07 13:12:08 +0700

    chore: move publishing to properties

--- build-logic/conventions/src/main/kotlin/convention.publish.gradle.kts
+++ build-logic/conventions/src/main/kotlin/convention.publish.gradle.kts
@@ -18,29 +18,29 @@ mavenPublishing {
 
     pom {
         name.set(project.name)
-        description.set(project.description ?: "Lscythe Version Catalog")
-        url.set("https://github.com/lscythe/gradle-version-catalog")
-        inceptionYear.set("2025")
+        description.set(project.description ?: providers.gradleProperty("POM_DESCRIPTION").getOrElse("Lscythe Version Catalog"))
+        url.set(providers.gradleProperty("POM_URL").getOrElse(""))
+        inceptionYear.set(providers.gradleProperty("POM_INCEPTION_YEAR").getOrElse("2025"))
 
         licenses {
             license {
-                name.set("MIT License")
-                url.set("https://opensource.org/licenses/MIT")
+                name.set(providers.gradleProperty("POM_LICENSE_NAME").getOrElse("MIT License"))
+                url.set(providers.gradleProperty("POM_LICENSE_URL").getOrElse("https://opensource.org/licenses/MIT"))
             }
         }
 
         developers {
             developer {
-                id.set("lscythe")
-                name.set("Lscythe")
-                url.set("https://github.com/lscythe")
+                id.set(providers.gradleProperty("POM_DEVELOPER_ID").getOrElse("lscythe"))
+                name.set(providers.gradleProperty("POM_DEVELOPER_NAME").getOrElse("Lscythe"))
+                url.set(providers.gradleProperty("POM_DEVELOPER_URL").getOrElse("https://github.com/lscythe"))
             }
         }
 
         scm {
-            url.set("https://github.com/lscythe/gradle-version-catalog")
-            connection.set("scm:git:git://github.com/lscythe/gradle-version-catalog.git")
-            developerConnection.set("scm:git:ssh://git@github.com/lscythe/gradle-version-catalog.git")
+            url.set(providers.gradleProperty("POM_SCM_URL").getOrElse(""))
+            connection.set(providers.gradleProperty("POM_SCM_CONNECTION").getOrElse(""))
+            developerConnection.set(providers.gradleProperty("POM_SCM_DEV_CONNECTION").getOrElse(""))
         }
     }
 }
@@ -49,7 +49,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/lscythe/gradle-version-catalogs")
+            url = uri(providers.gradleProperty("GITHUB_PACKAGES_URL").getOrElse(""))
             credentials {
                 username = providers.environmentVariable("GITHUB_ACTOR").orNull
                 password = providers.environmentVariable("GITHUB_TOKEN").orNull
@@ -57,4 +57,3 @@ publishing {
         }
     }
 }
-
